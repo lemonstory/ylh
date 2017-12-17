d37061f477d34ddeda69ace4eb3a96f94dd46175
gaoyong <gaoyong06@qq.com>
2017-12-17 09:55:59 +0800

    增加：餐饮要求

--- pages/API/food-require/food-require.js
+++ pages/API/food-require/food-require.js
@@ -1,143 +1,239 @@
 // pages/API/food-require/food-require.js
-Page({
+const app = getApp();
+const Toast = require('../../../zanui-weapp/dist/toast/index');
+var util = require('../../../utils/util.js')
+
+Page(Object.assign({}, Toast, {
 
   /**
    * 页面的初始数据
    */
   data: {
-    dishNum:1,
-    soupNum:1,
-    specialNum:1
-    },
-  back: function () {
-    wx.redirectTo({
-      url: "../car-require/car-require",
-    })
+    constant: app.constant,
+
+    currentFoodCourses: '',
+    currentFoodSoups: '',
+    currentFoodCharacteristic: '',
+    currentAppointValue: '',
   },
-  next: function () {
-    wx.redirectTo({
-      url: "../public-active/public-active",
-    })
-  },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+    var that = this;
+    //将上一页的数据在显示在当前页面
+    var pages = getCurrentPages();
+    var currPage = pages[pages.length - 1];   //当前页面
+    var prevPage = pages[pages.length - 2];  //上一个页面
+
+    var prevPageFormDataTemp = prevPage.data.formData;
+
+    var defaultFoodCourses = prevPageFormDataTemp.food.courses;
+    var defaultFoodSoups = prevPageFormDataTemp.food.soups;
+    var defaultFoodCharacteristic = prevPageFormDataTemp.food.characteristic;
+    var defaultFoodAppoint = prevPageFormDataTemp.food.appoint;
+
+    that.setData({
+      currentFoodCourses: defaultFoodCourses,
+      currentFoodSoups: defaultFoodSoups,
+      currentFoodCharacteristic: defaultFoodCharacteristic,
+      currentAppointValue: defaultFoodAppoint,
+    });
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
-  // 加减运算开始
-  bindMinus: function (e) {
-    var id = e.currentTarget.dataset.id;
-    if (id == "dish") {
-      var num = this.data.dishNum;
-      if (num >= 1) {
-        num--;
-      }
-      var minusStatus = num < 1 ? 'disabled' : 'normal';
-      this.setData({
-        dishNum: num,
-        minusStatus: minusStatus
-      });
-    } else if (id == "soup") {
-      var num = this.data.soupNum;
-      if (num >= 1) {
-        num--;
-      }
-      var minusStatus = num < 1 ? 'disabled' : 'normal';
-      this.setData({
-        soupNum: num,
-        minusStatus: minusStatus
-      });
-    } else if (id == "special") {
-      var num = this.data.specialNum;
-      if (num >= 1) {
-        num--;
-      }
-      var minusStatus = num < 1 ? 'disabled' : 'normal';
-      this.setData({
-        specialNum: num,
-        minusStatus: minusStatus
-      });
-    }
-
-
-
 
   },
-  bindPlus: function (e) {
-    var id = e.currentTarget.dataset.id;
-    if (id == "dish") {
-      var num = this.data.dishNum;
-      num++;
-      this.setData({
-        dishNum: num,
-      });
-    } else if (id == "soup") {
-      var num = this.data.soupNum;
-      num++;
-      this.setData({
-        soupNum: num,
-      });
+
+  // 计数器-减法运算开始
+  handleTapReduce: function (event) {
+
+    console.log("🍪 🍪 🍪 ️ ️️");
+    var that = this;
+    var id = event.currentTarget.id;
+
+    var pages = getCurrentPages();
+    var currPage = pages[pages.length - 1];   //当前页面
+    var prevPage = pages[pages.length - 2];  //上一个页面
+
+    var prevPageFormDataTemp = prevPage.data.formData;
+
+    switch (id) {
+
+      case 'courses':
+        if (prevPageFormDataTemp.food.courses >= 1) {
+          prevPageFormDataTemp.food.courses = prevPageFormDataTemp.food.courses - 1;
+          that.setData({
+            formData: prevPageFormDataTemp,
+            currentFoodCourses: prevPageFormDataTemp.food.courses
+          })
+        }
+        break;
+
+      case 'soups':
+        if (prevPageFormDataTemp.food.soups >= 1) {
+          prevPageFormDataTemp.food.soups = prevPageFormDataTemp.food.soups - 1;
+          that.setData({
+            formData: prevPageFormDataTemp,
+            currentFoodSoups: prevPageFormDataTemp.food.soups
+          })
+        }
+
+      case 'characteristic':
+        if (prevPageFormDataTemp.food.characteristic >= 1) {
+          prevPageFormDataTemp.food.characteristic = prevPageFormDataTemp.food.characteristic - 1;
+          that.setData({
+            formData: prevPageFormDataTemp,
+            currentCarsSites: prevPageFormDataTemp.food.characteristic
+          })
+        }
+        break;
     }
-    else if (id == "special") {
-      var num = this.data.specialNum;
-      num++;
-      this.setData({
-        specialNum: num,
-      });
-    }
+    // console.log(that.data.formData);
   },
 
+  //计数器-加法运算开始
+  handleTapIncrease: function (event) {
 
+    console.log("✈️ ️ ️️ ✈️ ️ ️️ ✈️ ️ ️️");
+    var that = this;
+    var id = event.currentTarget.id;
 
-})
+    var pages = getCurrentPages();
+    var currPage = pages[pages.length - 1];   //当前页面
+    var prevPage = pages[pages.length - 2];  //上一个页面
+
+    var prevPageFormDataTemp = prevPage.data.formData;
+
+    switch (id) {
+      case 'courses':
+        prevPageFormDataTemp.food.courses = prevPageFormDataTemp.food.courses + 1;
+        that.setData({
+          formData: prevPageFormDataTemp,
+          currentFoodCourses: prevPageFormDataTemp.food.courses
+        })
+        break;
+
+      case 'soups':
+        prevPageFormDataTemp.food.soups = prevPageFormDataTemp.food.soups + 1;
+        that.setData({
+          formData: prevPageFormDataTemp,
+          currentFoodSoups: prevPageFormDataTemp.food.soups
+        })
+        break;
+
+      case 'characteristic':
+        prevPageFormDataTemp.food.characteristic = prevPageFormDataTemp.food.characteristic + 1;
+        that.setData({
+          formData: prevPageFormDataTemp,
+          currentFoodCharacteristic: prevPageFormDataTemp.food.characteristic
+        })
+        break;
+    }
+    console.log(that.data.formData);
+  },
+
+  /**
+  * 指定车型
+  */
+  handleAppointInput: function (event) {
+
+    var value = event.detail.value;
+
+    var pages = getCurrentPages();
+    var currPage = pages[pages.length - 1];   //当前页面
+    var prevPage = pages[pages.length - 2];  //上一个页面
+
+    var prevPageFormDataTemp = prevPage.data.formData;
+    prevPageFormDataTemp.food.appoint = value;
+    prevPage.setData({
+      formData: prevPageFormDataTemp
+    })
+
+    console.log(prevPage.data.formData);
+  },
+
+  /**
+   * 上一步
+   */
+  handleTapPreStep: function () {
+    var that = this;
+    var url = '../car-require/car-require';
+    wx.redirectTo({
+      url: url,
+      success: function (res) { },
+      fail: function (res) {
+        that.showZanToast("页面跳转错误");
+      },
+      complete: function (res) { },
+    })
+  },
+
+  /**
+   * 
+   * 下一步
+   */
+  handleTapNextStep: function () {
+
+    var that = this;
+    var url = '../public-active/public-active';
+    wx.redirectTo({
+      url: url,
+      success: function (res) { },
+      fail: function (res) {
+        that.showZanToast("页面跳转错误");
+      },
+      complete: function (res) { },
+    })
+  }
+
+}));
